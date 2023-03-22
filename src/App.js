ec9f9816b6aaadb3f33b01f0b76e64bde84e39c4
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-03-22 21:38:39 +0100

    structure for contact

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-// import BaseButton from "./components/styles/Buttons";
-// import PrimaryButton from "./components/styles/PrimaryButton";
 import Layout from "./components/Layout";
 import ProductsDisplayed from "./components/Products";
 import IndividualProduct from "./components/IndividualProduct";
 import Div from "./components/styles/Div";
+import Contact from "./components/Contact";
 
 function Home() {
   return (
     <>
       <Div>
         <h1>Hello world!</h1>
-        {/* <h2>Second we testing</h2> */}
-        {/* <h3>Third stuff yes</h3>
-        <h4>This is a drill</h4> */}
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
@@ -26,8 +22,6 @@ function Home() {
           more recently with desktop publishing software like Aldus PageMaker
           including versions of Lorem Ipsum.
         </p>
-        {/* <BaseButton>Button</BaseButton>
-        <PrimaryButton>Primary</PrimaryButton> */}
         <ProductsDisplayed />
       </Div>
     </>
@@ -38,14 +32,6 @@ function Cart() {
   return (
     <Div>
       <h1>Cart</h1>
-    </Div>
-  );
-}
-
-function Contact() {
-  return (
-    <Div>
-      <h1>Contact Form</h1>
     </Div>
   );
 }
---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,11 @@
+import Div from "../styles/Div";
+
+function Contact() {
+    return (
+      <Div>
+        <h1>Contact Form</h1>
+      </Div>
+    );
+  }
+
+  export default Contact;
