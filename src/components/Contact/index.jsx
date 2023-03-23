532b9513f522c3f0a4f48802cf85d8313c4c2b89
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-03-23 12:46:51 +0100

    Added yup form validation + added styling

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,28 +1,48 @@
-import Div from "../styles/Div";
 import React from "react";
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import * as S from "./index.styled"
 
+const schema = yup
+.object({
+  name: yup.string().trim().min(3).max(30).required(),
+  email: yup.string().email().trim().min(3).max(40).required(),
+  subject: yup.string().trim().min(3).max(20).required(),
+  message: yup.string().trim().min(3).max(250).required(),
+})
+.required();
+
 function Contact() {
-    const { register, handleSubmit } = useForm();
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm({
+      resolver: yupResolver(schema),
+    });
 
     function onSubmit(data) {
-        console.log(data);
+      console.log(data);
     }
 
     return (
         <>
-        <Div>
+        <S.ContactDiv>
             <h1>Contact Form</h1>
             <p>Feel free to get in touch with Smart Cart by filling in the contact form.</p>
             <S.Form onSubmit={handleSubmit(onSubmit)}>
               <input placeholder="name"{...register('name')} />
+              <S.FormErrorMessage>{errors.name?.message}</S.FormErrorMessage>
               <input placeholder="email"{...register('email')} />
+              <S.FormErrorMessage>{errors.email?.message}</S.FormErrorMessage>
               <input placeholder="subject"{...register('subject')} />
+              <S.FormErrorMessage>{errors.subject?.message}</S.FormErrorMessage>
               <textarea placeholder="message" rows={10} cols={30}{...register('message')} />
-              <S.SubmitFormButton type="submit" />
+              <S.FormErrorMessage>{errors.message?.message}</S.FormErrorMessage>
+              <S.SubmitFormButton type="submit" value="Submit" />
             </S.Form>
-        </Div>
+        </S.ContactDiv>
       </>
     );
   }
--- src/components/Contact/index.styled.jsx
+++ src/components/Contact/index.styled.jsx
@@ -1,4 +1,11 @@
 import styled from 'styled-components';
+import Div from '../styles/Div';
+
+export const ContactDiv = styled(Div)`
+    @media(min-width: 620px) {
+        margin: auto;
+    }
+`;
 
 export const Form = styled.form`
     display: flex;
@@ -27,6 +34,7 @@ export const Form = styled.form`
 export const SubmitFormButton = styled.input`
     background: ${(props) => props.theme.color.accentOne};
     color: ${(props) => props.theme.color.white};
+    font-weight: 600;
     margin: 10px 0px;
     transition: 0.3s ease-in-out;
     cursor: pointer;
@@ -35,3 +43,9 @@ export const SubmitFormButton = styled.input`
         background: ${(props) => props.theme.color.secondary};
     }
 `; 
+
+export const FormErrorMessage = styled.p`
+    text-align: left;
+    color: #963a3a;
+    margin: 0px 5px 10px;
+`;
