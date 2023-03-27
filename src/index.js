94ce9bf5b33f23ce589169790b9f4a6a0990b3a9
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-03-27 17:59:34 +0200

    added lint-stage to the project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Theme } from "./components/defaultStyles/theme";
-import GlobalStyle from "./components/defaultStyles/GlobalStyle";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Theme } from './components/defaultStyles/theme';
+import GlobalStyle from './components/defaultStyles/GlobalStyle';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Theme>
