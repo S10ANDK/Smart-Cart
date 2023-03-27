62ed79c7a6b5fa44b4964a55b11710427b502acd
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-03-27 19:19:21 +0200

    fixed issue with empty stars

--- src/components/Stars/index.jsx
+++ src/components/Stars/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farEmptyStar } from '@fortawesome/free-regular-svg-icons';
 
 const GetStarRating = (rating) => {
   if (rating === 0) {
-    return [];
+    return <p>No yet rated</p>;
   }
 
   const fullStars = Math.floor(rating);
@@ -22,7 +23,7 @@ const GetStarRating = (rating) => {
   }
 
   for (let i = 0; i < emptyStars; i++) {
-    stars.push(<FontAwesomeIcon icon={faStar} key={`empty-${i}`} />);
+    stars.push(<FontAwesomeIcon icon={farEmptyStar} key={`empty-${i}`} />);
   }
 
   return stars;
