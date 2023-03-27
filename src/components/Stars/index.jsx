81dff0c3a92bdd5075cc6d1f9f78052f2c42b37c
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-03-27 18:11:42 +0200

    installed eslint

--- src/components/Stars/index.jsx
+++ src/components/Stars/index.jsx
@@ -1,5 +1,6 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 
 const GetStarRating = (rating) => {
   if (rating === 0) {
