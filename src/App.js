67c19c0c12c29fbd0ef0706ca4486c6ba4e2f99b
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-03-31 15:22:27 +0200

    progress on checkout success page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import IndividualProduct from './components/IndividualProduct';
 import Cart from './components/Cart';
 import Contact from './components/Contact';
+import CheckoutSuccess from './components/CheckoutSuccess';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="cart" element={<Cart />} />
+        <Route path="checkout-success" element={<CheckoutSuccess />} />
         <Route path="contact" element={<Contact />} />
         <Route path="products/:id" element={<IndividualProduct />} />
       </Route>
---
+++ src/components/CheckoutSuccess/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CheckoutSuccess() {
+  return <div>Success!</div>;
+}
+
+export default CheckoutSuccess;
