81dbc66954b2d5ea3bf5f03291b906895da2290d
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-03-31 17:11:38 +0200

    removed color

--- src/components/Cart/index.styled.jsx
+++ src/components/Cart/index.styled.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 
 export const CheckoutButton = styled(Link)`
   background: ${(props) => props.theme.color.accentThree};
-  /* color: ${(props) => props.theme.color.white}; */
   font-weight: 600;
   padding: 10px 20px;
   border-radius: 10px;
