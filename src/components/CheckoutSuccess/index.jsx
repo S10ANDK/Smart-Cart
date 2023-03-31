69002ec9121fe1180610253a9b945d3387c93c04
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-03-31 17:10:53 +0200

    improved checkout success page

--- src/components/CheckoutSuccess/index.jsx
+++ src/components/CheckoutSuccess/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Div from '../defaultStyles/Div';
+import { Link } from 'react-router-dom';
 
 function CheckoutSuccess() {
-  return <div>Success!</div>;
+  return (
+    <Div>
+      <h1>Success!</h1>
+      <p>You have successfully ordered your products!</p>
+      <Link to={'/'}>Continue shopping</Link>
+    </Div>
+  );
 }
 
 export default CheckoutSuccess;
