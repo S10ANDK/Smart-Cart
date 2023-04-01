8e9c12c7743197964a52611bea396b4d23d8116a
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-04-01 17:23:12 +0200

    added more mobile friendly margin

--- src/components/defaultStyles/Div.jsx
+++ src/components/defaultStyles/Div.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Div = styled.div`
   max-width: 1400px;
   min-height: 100vh;
-  margin: auto 10px;
+  margin: auto 20px;
 
   @media (min-width: 1020px) {
     margin: auto;
